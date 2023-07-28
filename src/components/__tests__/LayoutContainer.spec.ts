22d88237c405fb57abb8751dfadf4fcb7d62f76c
Wenwei Chang <silveryiris@gmail.com>
2023-07-28 23:00:16 +0800

    test: 更新單元測試

--- src/components/__tests__/LayoutContainer.spec.ts
+++ src/components/__tests__/LayoutContainer.spec.ts
@@ -1,11 +1,28 @@
 import { describe, it, expect } from "vitest"
 
-import { shallowMount } from "@vue/test-utils"
+import { mount } from "@vue/test-utils"
+import { createRouter, createWebHistory } from "vue-router"
 import LayoutContainer from "../LayoutContainer.vue"
+import { routes } from "@/router"
 
-describe("HeadNavBar", () => {
-  it("renders properly", () => {
-    const wrapper = shallowMount(LayoutContainer)
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+describe("Layout container renders properly", () => {
+  it("renders properly", async () => {
+    router.push("/")
+
+    await router.isReady()
+
+    const mountOptions = {
+      global: {
+        plugins: [router],
+      },
+    }
+
+    const wrapper = mount(LayoutContainer, mountOptions)
     expect(wrapper.isVisible()).toBeTruthy()
   })
 })
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
 
+export const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: { layout: "main" },
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../views/AboutView.vue"),
+    meta: { layout: "default" },
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-      meta: { layout: "main" },
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-      meta: { layout: "default" },
-    },
-  ],
+  routes,
 })
 
 export default router
