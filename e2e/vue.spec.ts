a3d9fa939960aced7889dbea0348523a459997ed
Wenwei Chang <silveryiris@gmail.com>
2023-08-04 21:18:42 +0800

    feat: 更新主要layout

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -3,17 +3,15 @@ import { test, expect } from "@playwright/test"
 // See here how to get started:
 // https://playwright.dev/docs/intro
 
-test("visits the app root url", async ({ page }) => {
+test("visits the app root url should be main layout", async ({ page }) => {
   await page.goto("/")
-  await expect(page.locator(".container.mx-auto")).toBeVisible()
+  await expect(page.locator("#main-layout")).toBeVisible()
 })
 
 test("switch layouts by page route meta", async ({ page }) => {
   await page.goto("/")
-  await expect(page.locator("#main-layout")).toHaveClass("@container/main container mx-auto p-4")
+  await expect(page.locator("#main-layout")).toHaveClass("@container/main drawer")
 
   await page.goto("/about")
-  await expect(page.locator("#default-layout")).toHaveClass(
-    "@container/default container mx-auto p-4"
-  )
+  await expect(page.locator("#default-layout")).toHaveClass("@container/default")
 })
--- src/components/HeadNavBar.vue
+++ src/components/HeadNavBar.vue
@@ -1,6 +1,14 @@
 <template>
-  <nav class="join my-2">
-    <RouterLink to="/" class="btn btn-primary join-item">Home</RouterLink>
-    <RouterLink to="/about" class="btn btn-primary join-item">About</RouterLink>
+  <nav class="join">
+    <RouterLink to="/" class="btn btn-secondary btn-sm join-item">
+      <HomeIcon class="h-4 w-4" />Home
+    </RouterLink>
+    <RouterLink to="/about" class="btn btn-secondary btn-sm join-item">
+      <TrophyIcon class="h-4 w-4" />About
+    </RouterLink>
   </nav>
 </template>
+
+<script setup lang="ts">
+import { HomeIcon, TrophyIcon } from "@heroicons/vue/20/solid"
+</script>
